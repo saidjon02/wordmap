c1b901c3d68a2f049df1e5939b46c9766c5e9b8a
Saidjon <fozilovsaidjon71@gmail.com>
2025-05-23 11:42:49 +0300

    Adddd gunicorn to requirements for Render deployy

--- wordmap/settings.py
+++ wordmap/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'core',  # Your core app
     'django.contrib.staticfiles',
 ]
-ALLOWED_HOSTS = ['https://wordmap-clu1.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://wordmap-clu1.onrender.com', 'localhost', '127.0.0.1', 'wordmap-clu1.onrender.com']
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 MIDDLEWARE = [
