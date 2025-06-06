2006b072f94fda66e4282640aa32d77eefa27ba2
Saidjon <fozilovsaidjon71@gmail.com>
2025-06-06 18:24:57 +0300

    seo inspectorr

--- wordmap/urls.py
+++ wordmap/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK", status=200)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', health_check),
     path('', include('core.urls')),  # Include the core app's URLs
 ]
